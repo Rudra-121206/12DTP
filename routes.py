41840ceeae98c143b3eab98693b207494f4bdf7c
none <patha@Rudraslaptop.com>
2023-07-08 00:36:24 +1200

    Showing menu page, working on customer form, menu page

--- routes.py
+++ routes.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template
+from flask_wtf import Flaskform
+from wtffroms_sqlalchemy.fields import QuerySelectField
+from wtforms import (StringField, TextAreaField, IntegerField, BooleanField,
+RadioField)
 
 app = Flask(__name__)
 
@@ -10,12 +14,23 @@ def home():
 
 @app.route("/menu")
 def menu():
+    conn = sqlite3.connect('Inventory management system.db')
+    cur = conn.cursor
+    cur.execute('SELECT * FROM Recipe ORDER BY name ASC;')
+    menu = cur.fetchall()
+    print(menu)
+    conn.close()
     return render_template("menu.html",title= "menu")
 
 
 
 @app.route("/admin")
 def admin():
+    conn = sqlite3.connect('Inventory management system.db')
+    cur = conn.cursor
+    cur.execute('SELECT * FROM inventory ORDER BY name ASC;')
+    inventory = cur.fetchall()
+    print(inventory)
     return render_template("admin.html",title= "admin")
 
 
@@ -25,6 +40,10 @@ def our_story():
     return render_template("our_story.html",title= "our_story")
     
     
+
+@app.route("customer_purchase")    
+def customer_purchase():
+
 
 
 
