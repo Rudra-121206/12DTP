b39b3a1e848df98bf17e3021538a7736b4eb81e0
none <patha@Rudraslaptop.com>
2023-08-22 23:42:09 +1200

    Changing form idea according to mr. dunford's inital plan

--- routes.py
+++ routes.py
@@ -19,6 +19,7 @@ def insert_database(*args):
     conn.close()
 
 
+
 # Call function and arguments should be in this order:
 # Statement -> OPTIONAL: arguments -> mode (1 = fetchall() and 2 = fetchone())
 def select_database(*args):
@@ -50,47 +51,59 @@ def menu():
     return render_template("menu.html", title="menu", menu=menu)
 
 
-# displays admin informmation
+# display admin informmation
 @app.route("/admin")
 def admin():
     inventory = select_database('SELECT * FROM Inventory ORDER BY \
 item_name ASC;', 1)
     return render_template("admin.html", title="admin", inventory=inventory)
 
-
+#display our story's page
 @app.route("/our story")
 def our_story():
     return render_template("our_story.html", title="our_story")
 
-
+#display form page
 @app.route("/customer_purchase", methods=['GET'])
 def customer_purchase():
     recipes = select_database('SELECT * FROM Recipe ORDER BY \
 recipe_name ASC;', 1)
     return render_template("costumer_purchase.html", recipes=recipes)
 
-
-@app.route("/customer_purchase", methods=['POST'])
+#get information from form
+@app.route("/customer_purchase", methods=['GET','POST'])
 def submit():
     name = request.form['name']
     phone = request.form['phone']
-    order = request.form.getlist('order')
+    qty = request.form.getlist('order')
+    qty = [int(y) for y in qty]
     insert_database('INSERT INTO Orders(order_name, phone_no) \
 VALUES (?,?)', name, phone)
     token = select_database('SELECT order_id FROM Orders ORDER BY \
 order_id DESC LIMIT 1', 2)
-    for i in order:
-        insert_database('INSERT INTO Recipe_Order(recipe_id, order_id) \
-VALUES (?,?)', i, token[0])
+    order = select_database('SELECT recipe_id FROM Recipe ORDER BY \
+recipe_name ASC;', 1)
+    print(order)
+    x = 0 
+    for i in qty:
+        if i > 0:
+            insert_database('INSERT INTO Recipe_Order(recipe_id, order_id, qty.) \
+VALUES (?,?)', order[x], token[0], i)
+        else:
+            pass
+        x=x+1    
         
     return redirect(url_for('success'))
 
+
+ 
 
 @app.route("/success", methods=['GET'])
 def success():
     token = select_database('SELECT order_id FROM Orders ORDER BY \
 order_id DESC LIMIT 1', 2)
     return (f"Your order is successful, your order token is {token[0]}")
+
 
 
 
